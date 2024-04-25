cc42adc8b52ed3406e101a6237b2928b3e36e8b1
George P <gpartskhaladzeg@gmail.com>
2024-04-25 16:55:23 +0400

    additional commit to prev commit

--- lib/ui/widgets/loader_screen/loader_screen_model.dart
+++ lib/ui/widgets/loader_screen/loader_screen_model.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:vk/domains/services/auth_service.dart';
-import 'package:vk/ui/navigation/navigator_service.dart';
-
-class LoaderScreenModel {
-  // final AuthService _authService = AuthService();
-  // init(BuildContext context) async {
-  //   if (_authService.user != null) {
-  //     await Navigator.of(context)
-  //         .pushReplacementNamed(MainNavigationRouteNames.mainScreen);
-  //   } else {
-  //     await Navigator.of(context)
-  //         .pushReplacementNamed(MainNavigationRouteNames.authScreen);
-  //   }
-  // }
-}
+class LoaderScreenModel {}
